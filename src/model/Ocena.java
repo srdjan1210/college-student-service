dd3b3663323fe9b01eb436efface2a4480f53bdc
Srdjan Stjepanovic <stjepanovic.ra168.2019@uns.ac.rs>
2021-11-05 11:42:48 +0100

    Added model classes(Predmet, Katedra, Ocena)

---
+++ src/model/Ocena.java
@@ -0,0 +1,56 @@
+package model;
+
+import java.util.Date;
+
+import utils.EnumTypes.BrOcena;
+
+public class Ocena {
+	private Student polozioIspit;
+	private Predmet predmet;
+	private BrOcena ocena;
+	private Date datumPolaganjaIspita;
+	
+	public Ocena(Student polozioIspit, Predmet predmet, BrOcena ocena, Date datumPolaganjaIspita) {
+		super();
+		this.polozioIspit = polozioIspit;
+		this.predmet = predmet;
+		this.ocena = ocena;
+		this.datumPolaganjaIspita = datumPolaganjaIspita;
+	}
+
+	public Student getPolozioIspit() {
+		return polozioIspit;
+	}
+
+	public void setPolozioIspit(Student polozioIspit) {
+		this.polozioIspit = polozioIspit;
+	}
+
+	public Predmet getPredmet() {
+		return predmet;
+	}
+
+	public void setPredmet(Predmet predmet) {
+		this.predmet = predmet;
+	}
+
+	public BrOcena getOcena() {
+		return ocena;
+	}
+
+	public void setOcena(BrOcena ocena) {
+		this.ocena = ocena;
+	}
+
+	public Date getDatumPolaganjaIspita() {
+		return datumPolaganjaIspita;
+	}
+
+	public void setDatumPolaganjaIspita(Date datumPolaganjaIspita) {
+		this.datumPolaganjaIspita = datumPolaganjaIspita;
+	}
+	
+	
+	
+	
+}
---
+++ src/utils/EnumTypes.java
@@ -0,0 +1,7 @@
+package utils;
+
+public class EnumTypes {
+	public static enum BrOcena {SEST, SEDAM, OSAM, DEVET, DESET};
+	public static enum Semestar {ZIMSKI, LETNJI};
+
+}
