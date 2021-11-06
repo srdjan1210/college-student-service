cbbb3dd7fbf57e27ad4889d8d7c6f05a72c5e7e0
Darko Selakovic <selakovic.ra157.2019@uns.ac.rs>
2021-11-06 22:30:12 +0100

    Fixed bug with icons

--- src/view/MenuBarComponent/MenuItems.java
+++ src/view/MenuBarComponent/MenuItems.java
@@ -1,24 +1,38 @@
 package view.MenuBarComponent;
 
-import javax.swing.ImageIcon;
 import java.awt.Image;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JMenuItem;
 import javax.swing.KeyStroke;
 import javax.swing.event.MenuKeyEvent;
+import utils.ImageUtils;
 
 public class MenuItems extends JMenuItem {
 	public MenuItems(String naziv, String iconURL, String shortcutText) {
 		super(naziv);
+		
 		if (iconURL != null) {
-			//Image icon = Toolkit.getDefaultToolkit().getImage(iconURL);
-			//icon = icon.getScaledInstance(13, 13, icon.SCALE_SMOOTH);
-			//setIcon(new ImageIcon(icon));
-			ImageIcon imageIcon=new ImageIcon(iconURL);
-			Image image=imageIcon.getImage().getScaledInstance(13,13,Image.SCALE_SMOOTH);
-			setIcon(new ImageIcon(image));
-
+			ImageIcon image=readImageIcon(iconURL);
+			image=ImageUtils.scaleImage(image, 13, 13);
+			setIcon(image);
 			char key = shortcutText.charAt(shortcutText.length() - 1);
 			setAccelerator(KeyStroke.getKeyStroke(key, MenuKeyEvent.CTRL_DOWN_MASK));
 		}
+	}
+	
+	public static ImageIcon readImageIcon(String path) {
+
+	    try {
+	        BufferedImage ic = ImageIO.read( ClassLoader.getSystemResource(path));
+	        ImageIcon ic2 = new ImageIcon(ic);
+	        return ic2;
+	    } catch (Exception e) {
+	        // TODO Auto-generated catch block
+	        e.printStackTrace();
+	    }
+	    return null;
 	}
 }
