686a33b2354f2f9db8b9ddb9c34b26a0521c3a84
Srdjan Stjepanovic <stjepanovic.ra168.2019@uns.ac.rs>
2021-11-07 18:06:29 +0100

    Changed layout of toolbar to default

--- src/view/ToolbarComponents/Toolbar.java
+++ src/view/ToolbarComponents/Toolbar.java
@@ -1,29 +1,65 @@
 package view.ToolbarComponents;
 
-import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 
-import javax.swing.JPanel;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JToolBar;
+import javax.swing.SwingConstants;
 
 import utils.Constants;
-import utils.PropertyFactory;
+import utils.ImageUtils;
 
-public class Toolbar extends JPanel {
+public class Toolbar extends JToolBar {
 
 	public Toolbar() {
-		super();
+		super(SwingConstants.HORIZONTAL);
 
-		ToolbarLeftSide tls = new ToolbarLeftSide();
-		ToolbarRightSide trs = new ToolbarRightSide();
+		setPreferredSize(new Dimension(Constants.SCREEN_WIDTH * 3 / 4, 30));
+		setFloatable(false);
 
-		setPreferredSize(new Dimension(Constants.SCREEN_WIDTH, 40));
-		setLayout(new BorderLayout());
-		PropertyFactory.addBlackBorder(this, 1);
+		ImageIcon iconNew = ImageUtils.scaleImage(ImageUtils.readImageIcon("Icons/NewMenuItem.png"), Constants.ICON_BUTTON_WIDTH, Constants.ICON_BUTTON_HEIGHT);
+		ImageIcon iconDelete = ImageUtils.scaleImage(ImageUtils.readImageIcon("Icons/DeleteMenuItem.png"), Constants.ICON_BUTTON_WIDTH, Constants.ICON_BUTTON_HEIGHT);
+		ImageIcon iconEdit = ImageUtils.scaleImage(ImageUtils.readImageIcon("Icons/EditMenuitem.png"), Constants.ICON_BUTTON_WIDTH, Constants.ICON_BUTTON_HEIGHT);
 
-		add(tls, BorderLayout.LINE_START);
-		add(trs, BorderLayout.LINE_END);
+		JButton btnNew = new JButton(iconNew);
+		JButton btnEdit = new JButton(iconEdit);
+		JButton btnDelete = new JButton(iconDelete);
+		JButton btnSearch = new JButton(iconEdit);
 
-		setPreferredSize(new Dimension(100, 40));
+		setButtonProperties(btnNew);
+		setButtonProperties(btnEdit);
+		setButtonProperties(btnDelete);
+		setButtonProperties(btnSearch);
+
+		// Left side of toolbar
+		add(new CustomSeparator());
+		add(btnNew);
+		add(new CustomSeparator());
+		add(btnEdit);
+		add(new CustomSeparator());
+		add(btnDelete);
+
+		// Middle
+		add(Box.createHorizontalGlue());
+
+		// Right side of toolbar
+		add(new ToolbarSearch());
+		add(new CustomSeparator());
+		add(btnSearch);
+		add(new CustomSeparator());
+
+	}
+
+	protected void setButtonProperties(JButton button) {
+		button.setSize(20, 20);
+		button.setBackground(Color.WHITE);
+		button.setBorder(BorderFactory.createEmptyBorder());
+		button.setContentAreaFilled(false);
+
 	}
 
 }
