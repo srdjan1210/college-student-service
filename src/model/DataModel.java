9b7c70f52fb874fdefe81fccaace69283406eba8
Srdjan Stjepanovic <stjepanovic.ra168.2019@uns.ac.rs>
2021-11-10 19:29:56 +0100

    Fixed null point exception in readProfessorsFromFile

--- src/model/DataModel.java
+++ src/model/DataModel.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -9,39 +10,55 @@ import utils.Constants.Status;
 import utils.EnumConversion;;
 
 public class DataModel {
+
 	private ArrayList<Student> students;
+	private ArrayList<Profesor> professors;
 
 	public DataModel() {
-		students = readStudentsFromFile();
-		System.out.println(students.size());
-		System.out.println(students.get(1).getDatumRodjenja());
-	}
-
-	private ArrayList<Student> readStudentsFromFile() {
-		ArrayList<Student> students = new ArrayList<Student>();
 		try {
-			File studentFile = new File("src/resources/studenti.txt");
-			Scanner scanner = new Scanner(studentFile);
-
-			while (scanner.hasNextLine()) {
-				String studentData = scanner.nextLine();
-				String[] data = studentData.split(",");
-				LocalDate date = LocalDate.parse(data[2]);
-				Status paymentType = EnumConversion.stringToStatus(data[9]);
-				
-				Student student = new Student(data[0], data[1], date, data[3], data[4], data[5], data[6],
-						Integer.parseInt(data[7]), Integer.parseInt(data[8]), paymentType,
-						Double.parseDouble(data[10]));
-				
-				students.add(student);
-			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
+			students = readStudentsFromFile();
+			professors = readProfessorsFromFile();
+		} catch (Exception err) {
+			err.printStackTrace();
 		}
 
+	}
+
+	private ArrayList<Student> readStudentsFromFile() throws FileNotFoundException {
+		ArrayList<Student> students = new ArrayList<Student>();
+		File studentFile = new File("src/resources/studenti.txt");
+		Scanner scanner = new Scanner(studentFile);
+
+		while (scanner.hasNextLine()) {
+			String studentData = scanner.nextLine();
+			String[] data = studentData.split(",");
+			LocalDate date = LocalDate.parse(data[2]);
+			Status paymentType = EnumConversion.stringToStatus(data[9]);
+
+			Student student = new Student(data[0], data[1], date, data[3], data[4], data[5], data[6],
+					Integer.parseInt(data[7]), Integer.parseInt(data[8]), paymentType, Double.parseDouble(data[10]));
+
+			students.add(student);
+		}
 		return students;
 
+	}
+
+	private ArrayList<Profesor> readProfessorsFromFile() throws FileNotFoundException {
+		ArrayList<Profesor> professors = new ArrayList<Profesor>();
+		File professorFile = new File("src/resources/profesori.txt");
+		Scanner scanner = new Scanner(professorFile);
+
+		while (scanner.hasNextLine()) {
+			String professorData = scanner.nextLine();
+			String[] data = professorData.split(",");
+			LocalDate date = LocalDate.parse(data[2]);
+			
+			Profesor professor = new Profesor(data[0], data[1], date, data[3], data[4], data[5], data[6], data[7],
+					data[8], Integer.parseInt(data[9]));
+			professors.add(professor);
+		}
+		return professors;
 	}
 
 }
