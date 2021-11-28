2d0d4a375384f7aa82d8644d007b4e4fd3b9d91f
Srdjan Stjepanovic <stjepanovic.ra168.2019@uns.ac.rs>
2021-11-28 21:15:32 +0100

    Added controller for adding new student

---
+++ src/controller/StudentWindowController.java
@@ -0,0 +1,56 @@
+package controller;
+
+import model.Address;
+import model.DataModel;
+import model.Student;
+import utils.Constants;
+import utils.EnumConversion;
+import view.ToolbarComponent.Student.ToolbarNewStudent;
+
+import javax.swing.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Vector;
+
+public class StudentWindowController {
+
+    public static void addNewStudent(ToolbarNewStudent window) {
+        if(checkIfFieldsEmpty(window)) {
+            Student student = createStudentObjectFromFields(window);
+            ArrayList<Student> students = DataModel.getInstance().getStudents();
+            students.add(student);
+            DataModel.getInstance().setStudents(students);
+        } else {
+            JOptionPane.showMessageDialog(window, "Polja nisu dobro popunjena");
+        }
+
+    }
+    private static Student createStudentObjectFromFields(ToolbarNewStudent studWin) {
+        String firstName = studWin.getTextField(0).getText();
+        String secondName = studWin.getTextField(1).getText();
+        LocalDate birthDate = LocalDate.parse(studWin.getTextField(2).getText());
+        Address address = createAddressFromAddressString(studWin.getTextField(3).getText());
+        String phoneNum = studWin.getTextField(4).getText();
+        String email = studWin.getTextField(5).getText();
+        String indexNum = studWin.getTextField(6).getText();
+        int startingYear = Integer.parseInt(studWin.getTextField(7).getText());
+        int currentYear = Integer.parseInt(studWin.getComboBox(8).getSelectedItem().toString());
+        Constants.Status financing = EnumConversion.stringToStatus(studWin.getTextField(0).getText());
+        return new Student(firstName, secondName, birthDate, address, phoneNum, email, indexNum, startingYear, currentYear, financing, 0);
+    }
+
+    private static Address createAddressFromAddressString(String addString) {
+        String[] addressParts = addString.split(":");
+        return new Address(addressParts[0],Integer.parseInt(addressParts[3]),addressParts[1],addressParts[2]);
+    }
+
+    private static boolean checkIfFieldsEmpty(ToolbarNewStudent window) {
+        Vector<JComponent> fields = window.getFieldsReferences();
+        for(int i = 0; i < fields.size() - 2; i++) {
+            JTextField field = (JTextField) fields.get(i);
+            if(field.getText().trim().equals("")) return false;
+        }
+
+        return true;
+    }
+}
---
+++ src/view/ToolbarComponent/ToolbarCustomComponents/ToolbarEnterExitPanel.java
@@ -0,0 +1,54 @@
+package view.ToolbarComponent.ToolbarCustomComponents;
+
+import controller.StudentWindowController;
+import view.ToolbarComponent.Student.ToolbarNewStudent;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ToolbarEnterExitPanel extends JPanel{
+
+	private JButton buttonConfirm;
+	private JButton buttonExit;
+	public ToolbarEnterExitPanel() {
+		super();
+		BoxLayout layout = new BoxLayout(this, BoxLayout.X_AXIS);
+		setLayout(layout);
+
+		buttonConfirm = new JButton("Potvrdi");
+		buttonConfirm.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//StudentWindowController.createStudentObjectFromFields((ToolbarNewStudent) buttonConfirm.getParent().getParent().getParent().getParent());
+				ToolbarNewStudent dialog = (ToolbarNewStudent) SwingUtilities.getWindowAncestor(buttonConfirm);
+				StudentWindowController.addNewStudent(dialog);
+			}
+		});
+		buttonExit = new JButton("Otkazi");
+		buttonExit.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Clicked");
+				//((ToolbarNewStudent)buttonExit.getParent().getParent().getParent().getParent()).dispose();
+				if(buttonExit.getParent().getParent() instanceof ToolbarNewStudent) {
+					System.out.println("This his toolbar new student");
+				} else {
+					System.out.println(buttonExit.getParent().getParent().getParent());
+				}
+			}
+		});;
+		add(buttonConfirm);
+		add(Box.createHorizontalStrut(50));
+		add(buttonExit);
+	}
+	
+	
+	public JButton getButtonConfirm() {
+		return buttonConfirm; 
+	}
+	
+	public JButton getButtonExit() {
+		return buttonExit;
+	}
+}
