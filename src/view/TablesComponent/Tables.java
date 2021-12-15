4388b8b55786c3dff1236152a680ff99781823e9
Srdjan Stjepanovic <stjepanovic.ra168.2019@uns.ac.rs>
2021-12-15 20:56:22 +0100

    Added selection properties in tables class

--- src/view/TablesComponent/Tables.java
+++ src/view/TablesComponent/Tables.java
@@ -1,11 +1,13 @@
 package view.TablesComponent;
 
-import javax.swing.JTable;
+import javax.swing.*;
 import javax.swing.table.AbstractTableModel;
 
 public class Tables extends JTable {
 	public Tables(AbstractTableModel tableModel) {
 		super();
 		setModel(tableModel);
+		setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		setColumnSelectionAllowed(false);
 	}
 }
