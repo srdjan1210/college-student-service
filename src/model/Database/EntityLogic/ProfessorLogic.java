2b57420e84c7129a902b667e445c81c73fd3afdf
Srdjan Stjepanovic <stjepanovic.ra168.2019@uns.ac.rs>
2021-12-29 00:13:16 +0100

    Refactored data model

---
+++ src/model/Database/EntityLogic/ProfessorLogic.java
@@ -0,0 +1,94 @@
+package model.Database.EntityLogic;
+
+import model.Database.DataModel;
+import model.Professor;
+import model.Subject;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ProfessorLogic {
+    private DataModel dataModel;
+    public ProfessorLogic(DataModel dm) {
+        dataModel = dm;
+    }
+
+    public void addProfessorToList(Professor newProfessor) {
+        ArrayList<Professor> professors = dataModel.getProfessors();
+        professors.add(newProfessor);
+        dataModel.notifyTable();
+    }
+
+    public Professor getProfessorById(String id) {
+        ArrayList<Professor> professors = dataModel.getProfessors();
+        for (Professor prof : professors) {
+            if (prof.getIdNumber().equals(id)) {
+                return prof;
+            }
+        }
+
+        return null;
+    }
+
+    public void setEditedProfessor(String oldId, Professor professorNewInfo) {
+        ArrayList<Professor> professors = dataModel.getProfessors();
+        for (int i = 0; i < professors.size(); i++) {
+            if (professors.get(i).getIdNumber().equals(oldId)) {
+                professors.get(i).setFirstName(professorNewInfo.getFirstName());
+                professors.get(i).setLastName(professorNewInfo.getLastName());
+                professors.get(i).setBirthDay(professorNewInfo.getBirthDay());
+                professors.get(i).setAddress(professorNewInfo.getAddress());
+                professors.get(i).setPhoneNumber(professorNewInfo.getPhoneNumber());
+                professors.get(i).setEmailAddress(professorNewInfo.getEmailAddress());
+                professors.get(i).setOfficeAddress(professorNewInfo.getOfficeAddress());
+                professors.get(i).setIdNumber(professorNewInfo.getIdNumber());
+                professors.get(i).setTitle(professorNewInfo.getTitle());
+                professors.get(i).setWorkingYears(professorNewInfo.getWorkingYears());
+                dataModel.notifyTable();
+            }
+        }
+    }
+
+    public boolean removeProfessorById(String id) {
+        // Treba dodati uklanjanje svih zavisnosti entiteta u drugim listama
+        ArrayList<Professor> professors = dataModel.getProfessors();
+        for (Professor professor : professors) {
+            if (professor.getIdNumber().equals(id)) {
+                professors.remove(professor);
+                removeProfessorDependencies(id);
+                dataModel.notifyTable();
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void removeProfessorDependencies(String id) {
+        removeProfessorFromSubjects(id);
+    }
+
+
+
+    private void removeProfessorFromSubjects(String id) {
+        ArrayList<Subject> subjects = dataModel.getSubjects();
+        for (Iterator<Subject> subIt = subjects.iterator(); subIt.hasNext(); ) {
+            Subject subject = subIt.next();
+
+            if (subject.getProfessor() == null) continue;
+            if (subject.getProfessor().getIdNumber().equals(id)) {
+                subject.setProfessor(null);
+            }
+        }
+    }
+
+    public String getProfessorIdFromEmail(String email) {
+        ArrayList<Professor> professors = dataModel.getProfessors();
+        for (Professor professor : professors) {
+            if (professor.getEmailAddress().equals(email)) {
+                return professor.getIdNumber();
+            }
+        }
+        return null;
+    }
+}
--- src/view/TablesComponent/Tables.java
+++ src/view/TablesComponent/Tables.java
@@ -2,6 +2,7 @@ package view.TablesComponent;
 
 import javax.swing.*;
 import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableCellRenderer;
 
 public class Tables extends JTable {
 	public Tables(AbstractTableModel tableModel) {
@@ -9,5 +10,14 @@ public class Tables extends JTable {
 		setModel(tableModel);
 		setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		setColumnSelectionAllowed(false);
+		centerText();
+	}
+
+	private void centerText() {
+		for(int i = 0; i < getModel().getColumnCount(); i++) {
+			DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+			centerRenderer.setHorizontalAlignment( JLabel.CENTER );
+			getColumnModel().getColumn(i).setCellRenderer( centerRenderer );
+		}
 	}
 }
