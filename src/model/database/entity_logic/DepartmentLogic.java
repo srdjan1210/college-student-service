7ae82152252b14184c874105d98c9e173d55e948
Srdjan Stjepanovic <stjepanovic.ra168.2019@uns.ac.rs>
2022-01-20 14:10:34 +0100

    Changed filter professor for department function

--- src/model/database/entity_logic/DepartmentLogic.java
+++ src/model/database/entity_logic/DepartmentLogic.java
@@ -24,25 +24,15 @@ public class DepartmentLogic {
     public ArrayList<Professor> filterProfessorForHeadOfDep(String depId) {
         ArrayList<Professor> filtered = new ArrayList<>();
         ArrayList<Professor> professors = dataModel.getProfessors();
-        for (Professor professor : professors) {
-            if (professor.getWorkingYears() > 5 && !professorIsHead(professor.getIdNumber(), depId)
-                    && professor.getTitle().toLowerCase().equals("redovni")
-                    || professor.getTitle().toLowerCase(Locale.ROOT).equals("vanredni_profesor")) {
+
+        for(Professor professor : professors) {
+            if(professor.getDepartment().equals(depId) && professor.getWorkingYears() > 5
+                && (professor.getTitle().toLowerCase().equals("redovni_profesor") || professor.getTitle().toLowerCase().equals("vanredni_profesor"))) {
                 filtered.add(professor);
             }
         }
 
         return filtered;
-    }
-
-    public boolean professorIsHead(String professorId, String depId) {
-        ArrayList<Department> departments = dataModel.getDepartments();
-        for(Department department: departments) {
-            if(department.getHeadOfTheDepartment() == null) continue;
-            if(department.getHeadOfTheDepartment().getIdNumber().equals(professorId) && department.getDepartmentId().equals(depId)) return false;
-            if(department.getHeadOfTheDepartment().getIdNumber().equals(professorId)) return true;
-        }
-        return false;
     }
 
     public void editDepartment(Department department, String oldDepId) {
